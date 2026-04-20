f1636ef454ab0d36e2f5a5fd1e71112195762a0b
Abhinav <satsyndicate@gmail.com>
2026-04-20 20:18:17 +0530

    perf(executor): add remote wake-up benchmark

---
+++ compio-executor/benches/remote.rs
@@ -0,0 +1,95 @@
+#![allow(dead_code, unused_variables, unused_assignments, unused_imports)]
+
+use std::{
+    hint::black_box,
+    pin::{Pin, pin},
+    sync::{Arc, Barrier},
+    task::{Context, Poll, Waker},
+};
+
+use compio_executor::{Executor, JoinError, JoinHandle};
+use criterion::{Criterion, criterion_group, criterion_main};
+use flume::{Receiver, Sender, bounded};
+
+std::thread_local! {
+    static EXE: Executor = Executor::new();
+}
+
+fn block_on<F: Future + 'static>(f: F) -> F::Output {
+    EXE.with(|exe| {
+        let cx = &mut Context::from_waker(Waker::noop());
+        let mut f = pin!(f);
+        loop {
+            if let Poll::Ready(res) = f.as_mut().poll(cx) {
+                return res;
+            }
+            exe.tick();
+        }
+    })
+}
+
+struct CounterFuture {
+    desired: usize,
+    current: usize,
+    shared: Sender<Waker>,
+}
+
+impl CounterFuture {
+    fn new(desired: usize, sender: Sender<Waker>) -> Self {
+        Self {
+            desired,
+            current: 0,
+            shared: sender,
+        }
+    }
+}
+
+impl Future for CounterFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.current += 1;
+        if self.current == 1 {
+            self.shared
+                .send(cx.waker().clone())
+                .expect("Must succeed as we send only once");
+        }
+        if self.current < self.desired {
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+}
+
+macro_rules! gen_func {
+    ($name:ident, $num:expr) => {
+        fn $name(c: &mut Criterion) {
+            let (tx, rx) = bounded::<Waker>(1);
+            std::thread::spawn(move || {
+                loop {
+                    if let Ok(t) = rx.recv() {
+                        (0..$num).for_each(|_| t.wake_by_ref());
+                    }
+                }
+            });
+
+            c.bench_function("FastSync Remote", |b| {
+                b.iter(|| {
+                    let cloned = tx.clone();
+                    block_on(black_box(async move {
+                        let counterfuture = CounterFuture::new($num, cloned);
+                        counterfuture.await;
+                    }))
+                })
+            });
+        }
+    };
+}
+
+gen_func!(bench_remote10, 10);
+gen_func!(bench_remote100, 100);
+gen_func!(bench_remote1000, 1000);
+
+criterion_group!(benches, bench_remote10, bench_remote100, bench_remote1000);
+criterion_main!(benches);
